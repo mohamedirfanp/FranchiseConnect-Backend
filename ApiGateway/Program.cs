6956359a5799ba00cf64e328d72da8779190f917
Mohamed Irfan P <pabi.adn1@gmail.com>
2023-07-07 22:56:39 +0530

    [Update] Added new Scoped Service for Chat Controller and Add config for AutoMapper

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -2,6 +2,7 @@
 using ApiGateway.Services.AccountService;
 using ApiGateway.Services.FranchiseService;
 using ApiGateway.GRPCMicroserviceClients;
+using ApiGateway.Services.ChatService;
 
 namespace ApiGateway
 {
@@ -11,17 +12,27 @@ namespace ApiGateway
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            
-
             // Get the IConfiguration instance
             IConfiguration configuration = builder.Configuration; 
 
             builder.Services.AddJwtAuthentication(configuration);
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigins", policy =>
+                {
+                    policy.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("http://localhost:3000")
+                    .AllowCredentials();
+                });
+            });
+
             // Add Client for GRPC Services
             builder.Services.AddSingleton<IGRPCClients, GRPCClients>();
 
             // Add services to the container.
+            builder.Services.AddHttpContextAccessor(); 
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,6 +40,7 @@ namespace ApiGateway
             builder.Services.AddScoped<IAccountService, AccountService>();
 
             builder.Services.AddScoped<IFranchiseService, FranchiseServiceApi>();
+            builder.Services.AddScoped<IChatService, ChatService>();
 
 
             builder.Services.AddSwaggerGen();
@@ -46,7 +58,7 @@ namespace ApiGateway
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+            app.UseCors("AllowOrigins");
 
             app.MapControllers();
 
