5f2723b817f44eac6f4d050f5af3337dc7077ab2
Mohamed Irfan P <pabi.adn1@gmail.com>
2023-07-10 22:17:18 +0530

    [ADD] Added grpc client for internal communication

---
+++ FranchiseGRPCService/GrpcServicesClient/GRPCClients.cs
@@ -0,0 +1,24 @@
+﻿using AccoutGRPCService.Protos;
+using ChatGRPCService.Protos;
+using Grpc.Net.Client;
+
+
+namespace FranchiseGRPCService.GrpcServicesClient
+{
+    public class GRPCClients : FranchiseGRPCService.GrpcServicesClient.IGRPCClients
+    {
+        public GRPCClients()
+        {
+            var accountGRPCServiceChannel = GrpcChannel.ForAddress("https://localhost:7212");
+            AccountClient = new AccountHandler.AccountHandlerClient(accountGRPCServiceChannel);
+
+            var chatGRPCServiceChannel = GrpcChannel.ForAddress("https://localhost:7291");
+            ConversationServiceClient = new ConversationService.ConversationServiceClient(chatGRPCServiceChannel);
+
+        }
+        public AccountHandler.AccountHandlerClient AccountClient { get; }
+
+        public ConversationService.ConversationServiceClient ConversationServiceClient { get; }
+    }
+
+}
---
+++ FranchiseGRPCService/GrpcServicesClient/IGRPCClients.cs
@@ -0,0 +1,18 @@
+﻿using AccoutGRPCService.Protos;
+using ChatGRPCService.Protos;
+
+namespace FranchiseGRPCService.GrpcServicesClient
+{
+    public interface IGRPCClients
+    {
+        
+        AccountHandler.AccountHandlerClient AccountClient { get; }
+
+        ConversationService.ConversationServiceClient ConversationServiceClient { get; }
+        /*        ConversationService.ConversationServiceClient ConversationServiceClient { get; }
+
+                QueryService.QueryServiceClient QueryServiceClient { get; }
+
+                ChatService.ChatServiceClient ChatServiceClient { get; }*/
+    }
+}
